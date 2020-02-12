8f6e998a447d75414f708a1d7d478a359d60bd41
pcmxgti <16561338+pcmxgti@users.noreply.github.com>
2020-02-12 10:15:26 -0500

    Minimal terraform to create roles for testing

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region  = var.region
+  version = "~> 2.0"
+}
+
+provider "local" {
+  version = "~> 1.4"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
